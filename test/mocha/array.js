69f61efcc6a924c82490fade20b1cd9597c9e900
Dávid Hezký <hezky@epos.cd.cz>
2019-04-02 15:28:07 +0200

    add: test array equal

--- test/mocha/array.js
+++ test/mocha/array.js
@@ -1,6 +1,72 @@
 import { assert } from "chai";
 import { utilsArray } from "";
 
+describe("boolean : method equal", () => {
+  it("equal : true (boolean)", () => {
+    const a1 = [true, true, false, false];
+    const a2 = [true, true, false, false];
+    assert.ok(
+      utilsArray.equal(a1, a2),
+      "equal([true, true, false, false],[true, true, false, false]) === true (boolean)"
+    );
+  });
+  it("equal : true (date)", () => {
+    const a1 = [new Date(2016, 1, 1), new Date(2016, 2, 1), new Date(2016, 3, 1), new Date(2016, 4, 1)];
+    const a2 = [new Date(2016, 1, 1), new Date(2016, 2, 1), new Date(2016, 3, 1), new Date(2016, 4, 1)];
+    assert.ok(utilsArray.equal(a1, a2), "equal array with dates");
+  });
+  it("equal : true (numbers)", () => {
+    const a1 = [1, 2, 3, 4];
+    const a2 = [1, 2, 3, 4];
+    assert.ok(utilsArray.equal(a1, a2), "equal([1,2,3,4],[1,2,3,4]) === true (numbers)");
+  });
+  it("equal : true (regexp)", () => {
+    const a1 = [/ab+c/, /ab+d/, /ab+e/, /ab+f/];
+    const a2 = [/ab+c/, /ab+d/, /ab+e/, /ab+f/];
+    assert.ok(utilsArray.equal(a1, a2), "equal array with regexp");
+  });
+  it("equal : true (strings)", () => {
+    const a1 = ["1", "2", "3", "4"];
+    const a2 = ["1", "2", "3", "4"];
+    assert.ok(utilsArray.equal(a1, a2), "equal(['1','2','3','4'],['1','2','3','4']) === true (strings)");
+  });
+  it("equal : true (mixed)", () => {
+    const a1 = [1, /ab+d/, "tri", new Date(2016, 1, 1)];
+    const a2 = [1, /ab+d/, "tri", new Date(2016, 1, 1)];
+    assert.ok(utilsArray.equal(a1, a2), "equal array with mixed");
+  });
+  it("equal : true (deep)", () => {
+    const a1 = [1, 1, 2, [1, 2, 3, 4], 5, 6];
+    const a2 = [1, 1, 2, [1, 2, 3, 4], 5, 6];
+    assert.ok(utilsArray.equal(a1, a2), "equal array with deep");
+  });
+  it("equal : false (numbers)", () => {
+    const a1 = [1, 2, 3, 4];
+    const a2 = [1, 2, 3, 4, 5];
+    assert.notOk(utilsArray.equal(a1, a2), "not equal array with numbers");
+  });
+  it("equal : false (numbers)", () => {
+    const a1 = [1, 2, 3, 4];
+    const a2 = [4, 1, 2, 3];
+    assert.notOk(utilsArray.equal(a1, a2), "not equal array with numbers");
+  });
+  it("equal : false (undefined)", () => {
+    const a1 = undefined;
+    const a2 = undefined;
+    assert.notOk(utilsArray.equal(a1, a2), "bad parameters undefined");
+  });
+  it("equal : false (null)", () => {
+    const a1 = null;
+    const a2 = null;
+    assert.notOk(utilsArray.equal(a1, a2), "bad parameters null");
+  });
+  it("equal : false (bad parameters)", () => {
+    const a1 = 1;
+    const a2 = [];
+    assert.notOk(utilsArray.equal(a1, a2), "bad parameters");
+  });
+});
+
 describe("array : method is", () => {
   it("is : true", () => {
     assert.isTrue(utilsArray.is([]), "is([]) === true");
