983485be85fe168d0fcf729f80af8927a0bed6c3
Dávid Hezký <david.hezky@cdis.cz>
2022-05-01 19:34:07 +0200

    rm: remove temp folder

--- temp/abc/types/boolean/copy.js
+++
@@ -1,9 +0,0 @@
-import { assert } from "chai";
-import { utilsBoolean } from "";
-
-describe("boolean : method copy", () => {
-  it("copy", () => {
-    assert.equal(utilsBoolean.copy(false), false, "copy(false) === false");
-    assert.equal(utilsBoolean.copy(true), true, "copy(true) === true");
-  });
-});
--- temp/abc/types/string/copy.js
+++
@@ -1,8 +0,0 @@
-import { assert } from "chai";
-import { utilsString } from "";
-
-describe("string : method copy", () => {
-  it("copy", () => {
-    assert.equal(utilsString.copy("ABC"), "ABC", "copy(ABC) === ABC");
-  });
-});
