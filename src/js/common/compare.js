b481de75b864c16b3d04968e947b36c157a7c8ea
Dávid Hezký <david.hezky@cdis.cz>
2022-05-04 20:58:17 +0200

    fix: refactoring - method compare

--- src/js/common/compare.js
+++ src/js/common/compare.js
@@ -1,11 +1,44 @@
 import { TYPE_ERROR_MESSAGE, UTILS_LIST } from "common/constants";
 
-const compare = (a, b, dataTypes = UTILS_LIST) => {
-  const utils = dataTypes.find((item) => item.is(a) || item.is(b));
-  const { is, compare } = utils;
-  if (utils === undefined || !is || !compare)
-    throw new TypeError(TYPE_ERROR_MESSAGE);
-  return is(a) && is(b) ? compare(a, b) : is(b) - is(a);
+//------------------------------------------------------------------------------
+const compareDiffTypes = (a, b, foundType) => {
+  const isType = foundType.is;
+  return isType(b) - isType(a);
 };
+
+const equalTypes = (a, b, foundType) => {
+  const isType = foundType.is;
+  return isType(a) && isType(b);
+};
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+const compareByType = (a, b, foundType) => {
+  let res = null;
+  if (equalTypes(a, b, foundType)) {
+    const compareSameTypes = foundType.compare;
+    res = compareSameTypes(a, b);
+  } else {
+    res = compareDiffTypes(a, b, foundType);
+  }
+  return res;
+};
+
+const findType = function (itemType) {
+  const isType = itemType.is;
+  return isType(this.a) || isType(this.b);
+};
+
+const notFoundType = (foundType) =>
+  !foundType || !foundType.is || !foundType.compare;
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+const compare = (a, b, dataTypes = UTILS_LIST) => {
+  const foundType = dataTypes.find(findType, { a, b });
+  if (notFoundType(foundType)) throw new TypeError(TYPE_ERROR_MESSAGE);
+  return compareByType(a, b, foundType);
+};
+//------------------------------------------------------------------------------
 
 export default compare;
