cdd545013acfa3a8b71e29a2a58bffde0ae46de1
Dávid Hezký <david.hezky@cdis.cz>
2022-10-25 21:47:17 +0200

    fix: sonar - test regExp

--- test/mocha/types/regExp/checkTypes/default.js
+++ test/mocha/types/regExp/checkTypes/default.js
@@ -5,20 +5,18 @@ import { TYPE_ERROR_MESSAGE } from "common/constants";
 describe("regExp : method checkTypes", () => {
   it("result ... true", () => {
     const regExp1 = /\w+/;
-    const regExp2 = new RegExp("ab+c", "i");
-    const regExp3 = new RegExp(/ab+c/, "i");
+    const regExp2 = new RegExp(/ab+c/, "i");
     assert.isTrue(
-      utilsRegExp.checkTypes(regExp1, regExp2, regExp3),
+      utilsRegExp.checkTypes(regExp1, regExp2),
       'checkTypes("A","B","C") === true'
     );
   });
   it("result ... throw error", () => {
     const regExp1 = /\w+/;
-    const regExp2 = new RegExp("ab+c", "i");
-    const regExp3 = 2;
+    const regExp2 = 2;
     assert.throws(
       function () {
-        utilsRegExp.checkTypes(regExp1, regExp2, regExp3);
+        utilsRegExp.checkTypes(regExp1, regExp2);
       },
       Error,
       TYPE_ERROR_MESSAGE
